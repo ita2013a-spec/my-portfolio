ea118ccadc12a093514e2979103ef4626b8c212f
ita2013a-spec <ita2013a@gmail.com>
2026-05-15 15:17:38 +0300

    Delete formdata.php

--- formdata.php
+++
@@ -1,4 +0,0 @@
-<?php
-mail("ita-win8@hotmail.com", "Message from visit", "User: $_POST[name] \nContent: $_POST[text]", "Content-type: text/plain; charset=utf-8");
-?>
-<FORM><INPUT type=button value="Your message has been sent" OnClick="location.href='guest_book.html'"></FORM>
